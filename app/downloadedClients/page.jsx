8029a3c5eea8ae6941d78d6498d0afde4f5947dd
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-26 16:42:29 +0530

    clientMode2

--- app/downloadedClients/page.jsx
+++ app/downloadedClients/page.jsx
@@ -77,7 +77,7 @@ const totalPfCountt = Pfstock()
         <div>
             <h1 className="text-xl font-bold mb-4">List of clients having Invoices generated</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {invoices.map((invoice) => (
+                {invoices.map((invoice, index) => (
                     invoice.invCount && invoice.invCount > 0 ? (
                       <Link
               href={`/getOneInvoice/${invoice._id}`}
